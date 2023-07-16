a13505380f1b602cc6842236037eb219aefd1149
SaraS <sarasaco@ucm.es>
2023-07-16 17:59:39 +0200

    subidos ficheros interacción con el entorno: puertas

---
+++ TFG-new-project/Assets/Scripts/doorTagEditor.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(GameObject))]
+public class doorTagEditor : Editor
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnEnable()
+    {
+        if (Selection.activeGameObject != null && Selection.activeGameObject.CompareTag("Door"))
+        {
+            // Verifica si el componente ya existe antes de agregarlo
+            if (Selection.activeGameObject.GetComponent<doorController>() == null)
+            {
+                Selection.activeGameObject.AddComponent<doorController>();
+            }
+
+            // Verifica si el collider ya existe antes de agregarlo
+            if (Selection.activeGameObject.GetComponent<BoxCollider>() == null)
+            {
+                Selection.activeGameObject.AddComponent<BoxCollider>();
+            }
+        }
+    }
+}
