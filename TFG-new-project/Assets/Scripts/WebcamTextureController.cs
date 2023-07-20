e3c4b1f2f6b012ff1e4d269e3a9b561a01fe4e2e
SaraS <sarasaco@ucm.es>
2023-07-20 19:23:47 +0200

    vista de webcam añadida
    :D

---
+++ TFG-new-project/Assets/Scripts/WebcamTextureController.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WebcamTextureController : MonoBehaviour
+{
+    public RawImage webcamDisplay; // Referencia a la Raw Image para mostrar la vista de la webcam
+    private WebCamTexture webcamTexture;
+
+    void Start()
+    {
+        // Buscar la referencia a la Raw Image en el Canvas
+        webcamDisplay = GameObject.FindObjectOfType<RawImage>();
+
+        // Asegúrate de que haya al menos una webcam disponible
+        if (WebCamTexture.devices.Length == 0)
+        {
+            Debug.LogError("No se ha encontrado ninguna webcam.");
+            return;
+        }
+
+        // Obtén la primera webcam disponible (puedes ajustar esto según tus necesidades)
+        webcamTexture = new WebCamTexture(WebCamTexture.devices[0].name);
+
+        // Inicia la vista de la webcam
+        webcamTexture.Play();
+
+        // Asigna la vista de la webcam a la Raw Image
+        if (webcamDisplay != null)
+        {
+            webcamDisplay.texture = webcamTexture;
+        }
+    }
+
+    void OnDestroy()
+    {
+        // Detén la vista de la webcam cuando el script se destruye
+        if (webcamTexture != null && webcamTexture.isPlaying)
+        {
+            webcamTexture.Stop();
+        }
+    }
+}
