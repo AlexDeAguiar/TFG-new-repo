b2830a8a934b021e0832a33c3735ab996713e639
AlexDeAguiar <alexdeag@ucm.es>
2023-07-24 19:57:34 +0200

    coherence

--- TFG-new-project/Assets/Scripts/SC_TPSController.cs
+++ TFG-new-project/Assets/Scripts/SC_TPSController.cs
@@ -7,7 +7,7 @@ public class SC_TPSController : MonoBehaviour
     public float speed = 7.5f;
     public float jumpSpeed = 8.0f;
     public float gravity = 20.0f;
-    public Transform playerCameraParent;
+    public GameObject playerCameraParent;
     public float lookSpeed = 2.0f;
     public float lookXLimit = 60.0f;
 
@@ -21,7 +21,9 @@ public class SC_TPSController : MonoBehaviour
     void Start()
     {
         characterController = GetComponent<CharacterController>();
-        rotation.y = transform.eulerAngles.y;
+		playerCameraParent =Instantiate(playerCameraParent, transform);
+
+		rotation.y = transform.eulerAngles.y;
     }
 
     void Update()
@@ -55,7 +57,7 @@ public class SC_TPSController : MonoBehaviour
             rotation.y += Input.GetAxis("Mouse X") * lookSpeed;
             rotation.x += -Input.GetAxis("Mouse Y") * lookSpeed;
             rotation.x = Mathf.Clamp(rotation.x, -lookXLimit, lookXLimit);
-            playerCameraParent.localRotation = Quaternion.Euler(rotation.x, 0, 0);
+            playerCameraParent.transform.localRotation = Quaternion.Euler(rotation.x, 0, 0);
             transform.eulerAngles = new Vector2(0, rotation.y);
         }
     }
